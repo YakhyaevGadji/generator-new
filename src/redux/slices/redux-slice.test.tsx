a02fa52f6685aa8257a6d0afec7258ff6e0a6220
YakhyaevGadji <gadji180105@gmail.com>
2024-10-03 13:12:14 +0300

    add: test

--- src/redux/slices/redux-slice.test.tsx
+++ src/redux/slices/redux-slice.test.tsx
@@ -67,54 +67,6 @@ describe('redux reducers', () => {
         expect(nextState.cards[0].status).toBe(true);
     });
 
-    it('setSelectedCards test check true status', () => {
-        const stateWithSelectedCard: Data = {
-            cards: [
-                {
-                    id: 1,
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2nAnjJlvd4VGFaoKIyredFj6o66AFuevc7A&s',
-                    title: 'kit one1',
-                    kit: 'one',
-                    status: false,
-                },
-                {
-                    id: 2,
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlgMO_vLuojLTA7Jmm4XBQmSuF0ykBMXZVig&s',
-                    title: 'kit one2',
-                    kit: 'one',
-                    status: false,
-                },
-            ],
-
-            buttons: [
-                { id: 1, kit: 'one', title: 'Набор 1', count: 4 },
-                { id: 2, kit: 'two', title: 'Набор 2', count: 5 },
-            ],
-
-            // Массив выбранных карточек
-            selectedCards: [
-                {
-                    id: 1,
-                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2nAnjJlvd4VGFaoKIyredFj6o66AFuevc7A&s',
-                    title: 'kit one1',
-                    kit: 'one',
-                    status: false,
-                },
-            ],
-
-            //Объект с данными кнопок
-            buttonData: {
-                kit: '',
-                count: 0,
-            },
-        };
-
-        const nextState = reducer(stateWithSelectedCard, setSelectedCards({ card: stateWithSelectedCard.cards[0], id: 1 }));
-
-        expect(nextState.selectedCards).toEqual([]);
-        expect(nextState.cards[0].status).toBe(true);
-    });
-
     it('resetSelectedCards', () => {
         expect(store.dispatch(resetSelectedCards())).toEqual({payload: undefined, type: "data/resetSelectedCards"});
     });
